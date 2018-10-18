84241201af8268f68074aaa3836a317f6b063837
marcelomn <marcelomendesn@gmail.com>
2018-10-18 18:31:03 -0300

    cabecalho

---
+++ application/views/site/_inc/cabecalho.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="{base_url}assets/site/img/bras.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="{base_url}assets/site/img/bras.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>Brascont Inteligência Contábil </title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+    <!--  Bootstrap CSS -->
+    <link href="{base_url}assets/site/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="{base_url}assets/site/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
+    <link href="{base_url}assets/site/css/styleCustomizado.css" rel="stylesheet"/>
+    <link href="{base_url}assets/site/css/demo.css" rel="stylesheet" />
+    <link href="{base_url}assets/site/css/style.css" rel="stylesheet" type="text/css" />
+
+    <!--  Fonts e icons  -->
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href="{base_url}assets/site/css/nucleo-icons.css" rel="stylesheet">
+
+    <!--  JS Files -->
+    <script src="{base_url}assets/site/js/jquery-3.2.1.js" type="text/javascript"></script>
+    <script src="{base_url}assets/site/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+    <script src="{base_url}assets/site/js/popper.js" type="text/javascript"></script>
+    <script src="{base_url}assets/site/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="{base_url}assets/site/js/paper-kit.js?v=2.1.0"></script>
+    <script src="{base_url}assets/site/js/bootstrap-switch.min.js"></script>
+    <script src="{base_url}assets/site/js/nouislider.js"></script>
+    <script src="{base_url}assets/site/js/moment.min.js"></script>
+    <script src="{base_url}assets/site/js/bootstrap-datetimepicker.min.js"></script>
+    <!--<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>-->
+    <script type="text/javascript" src="{base_url}assets/site/js/jquery.flexisel.js"></script>
+    <script src="{base_url}assets/site/js/script-customizado.js"></script>
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="500">
+        <div class="container">
+            <div class="navbar-translate">
+                <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-bar"></span>
+                    <span class="navbar-toggler-bar"></span>
+                    <span class="navbar-toggler-bar"></span>
+                </button>
+                <a href ="{base_url}"><img class="navbar-brand" src="{base_url}assets/site/img/LogoBrascont_Branco.png"/></a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav ml-auto{class_menu_internas}">
+                    <li class="nav-item">
+                        <a class="nav-link" rel="tooltip" href="{base_url}">home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" rel="tooltip" href="{base_url}empresa">empresa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" rel="tooltip" href="{base_url}areas-de-atuacao">áreas de atuação</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" rel="tooltip" href="{base_url}links-uteis">links úteis</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" rel="tooltip" href="{base_url}contato">contato</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+        
